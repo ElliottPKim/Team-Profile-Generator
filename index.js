35960d80e391c27a1f0135e03eeb4f358e3344d1
Elliott Kim <elliottkim2@hotmail.com>
2022-03-01 19:08:32 -0800

    Created constructors and tested them

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+const generateHTML = require('./src/generateHTML');
+const fs = require('fs'); 
+
+teamArray = [];
+
